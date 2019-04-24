a9d8cc25872949b9af0b26afe3b300a370f30d4b
HuangRicky <Ricky.Huang2@garmin.com>
2019-04-24 13:08:45 +0800

    add success and error method, update version

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
+### introduction
+show alert message on your webpage
 ### usage
 npm install -D alert-message
 
 ```
 import Message from "alert-message"
 
-// methods
- 
-Message.warn()
+/**
+ * @param args - {txt: "", time: 2500}, time default 2500(ms)
+ */
+Message.warn({txt: "warning text"});
+Message.error({txt: "error text"});
+Message.success({txt: "success text"});
 ```
--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
+const warnImg = require("./images/warn.png"),
+    errorImg = require("./images/error.png"),
+    successImg = require("./images/success.png"),
+    IMG = {
+        warn: warnImg,
+        error: errorImg,
+        success: successImg
+    };
 class Message {
     constructor () {
-        this.fadeTime = 2000;
+        this.fadeTime = 2500;
         this.messageContainer = null;
     }
 
     renderDom (type, args) {
+
         if (!this.messageContainer) {
             this.messageContainer = document.createElement("div");
             this.messageContainer.className = "r-message-container";
         }
-        let divOuter = document.createElement("div"), div = document.createElement("div");
-        div.className = "r-" + type;
-        divOuter.className = "r-" + type + "-outer";
-        div.innerText = args.txt;
+        let divOuter = document.createElement("div"),
+            div = document.createElement("div"),
+            img = document.createElement("img"),
+            span = document.createElement("span");
+        div.className = "r-alert";
+        img.className = "r-alert-img";
+        if (IMG[type]) {
+            img.src = IMG[type];
+        } else {
+            img.src = IMG.warn;
+        }
+        span.innerText = args.txt;
+        span.className = "r-warn-txt";
+        div.appendChild(img);
+        divOuter.className = "r-alert-outer";
+        div.appendChild(span);
         divOuter.appendChild(div);
         this.messageContainer.appendChild(divOuter);
         document.querySelector("body").appendChild(this.messageContainer);
@@ -21,8 +42,18 @@ class Message {
         }, args.time || this.fadeTime);
     }
 
-    warn (args = {txt: "", time: 2000}) {
+    /**
+     *
+     * @param args - {txt: "", time: 2500}
+     */
+    warn (args = {txt: "", time: 2500}) {
         this.renderDom("warn", args);
+    }
+    error (args = {txt: "", time: 2500}) {
+        this.renderDom("error", args);
+    }
+    success (args = {txt: "", time: 2500}) {
+        this.renderDom("success", args);
     }
 }
 
